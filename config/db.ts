4155551a5eb8624fc8fabbd86e4ffb83839bd29f
yangbeibei88 <beibeiyang88@hotmail.com>
2024-11-05 17:11:14 +1000

    db connection close

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,10 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 const uri = Deno.env.get("MONGO_URI")!;
+const dbName = Deno.env.get("MONGO_DBNAME");
 console.log(uri);
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -13,9 +15,18 @@ const client = new MongoClient(uri, {
 
 export const connectDB = async () => {
   try {
+    // connect the client to the server
     await client.connect();
+
+    // send a ping to confirm a successful connection
+    await client.db(dbName).command({ ping: 1 });
     console.log("DB connected!");
   } catch (error) {
     console.error(error);
+  } finally {
+    // Because Node.js driver auto calls MongoClient.connect() when perform CRUD
+    // when the API initially connect the MongoDB, explicitly verify if the connection is resolved
+    // The client will close if finish/error
+    await client.close();
   }
 };
